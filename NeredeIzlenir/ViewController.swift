e2ea522bd0111d567cf073efcdcb64e444116207
Tahir Uzelli <>
2022-06-21 18:12:49 +0300

    project init

---
+++ NeredeIzlenir/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NeredeIzlenir
+//
+//  Created by Tahir Uzelli on 21.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
